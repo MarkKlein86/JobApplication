3187880b0185457774fda6ba3adb14f34fcfe767
Endirell <endirell@gmail.com>
2019-03-03 19:06:13 +0100

    feat(routes/cv.js): add standard script

--- NodeJsTestApp/NodeJsTestApp/app.js
+++ NodeJsTestApp/NodeJsTestApp/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var cv = require('./routes/cv');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/cv', cv);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
